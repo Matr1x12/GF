1bd780b221727260d62c5fc6d8b47e0cf0a583f7
FisnikUka <fu69968@ubt-uni.net>
2024-12-07 23:27:30 +0100

    krijim i  login.html me validation duke perdorur login.js

---
+++ Project/js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+        alert('Please fill in all fields.');
+        return;
+    }
+
+    alert('Login successful!');
+});
